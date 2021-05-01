5d3de91a0ae1126303505491bc671f98f760c161
Yusuke Kawasaki <u-suke@kawa.net>
2021-05-01 13:28:44 +0900

    async define

--- browser/wrap.js
+++ browser/wrap.js
@@ -1,14 +1,24 @@
-var Mustatte = (function(load, main) {
-  var defined = {};
-  if ("undefined" != typeof exports) defined[main] = exports;
-  var require = defined.require = function(name) {
-    return defined[name]
-  };
-  load(function(name, deps, fn) {
-    defined.exports = defined[name] || (defined[name] = {});
-    fn.apply(null, deps.map(require));
-  });
-  return defined[main];
+var Mustatte = (function(init, main) {
+  var mod = {require: require};
+  var def = {};
+  var pre = {};
+  if ("undefined" != typeof exports) pre[main] = exports;
+  define.amd = true;
+  init(define);
+  return require(main);
+
+  function require(name) {
+    return mod[name] || (mod[name] = def[name]());
+  }
+
+  function define(name, deps, fn) {
+    def[name] = function() {
+      var exports = mod.exports = pre[name] || {};
+      var module = mod.module = {exports: exports};
+      fn.apply(null, deps.map(require));
+      return module.exports;
+    }
+  }
 })(function(define) {
   // AMD
 }, "index"); // END
